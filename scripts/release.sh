87e0bc61c9d544db51265ec013161404c8827daf
chuck <15524415221@163.com>
2024-08-01 02:02:13 +0800

    feat: 调整版本内容，添加发行脚本,发布1.0.1版本

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #发行新的版本上传到中心仓库可以执行这个脚本
-version=1.0.1.RELEASE
+version=1.0.1
 cd ../
 cd structure-cloud-dependencies
 mvn clean deploy -P release,oss -Dmaven.test.skip=true -Drevision=$version
